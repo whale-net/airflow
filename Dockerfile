a935fc5c96bad58044af22cfc9eeebf71f3bcb0d
qmastermoo <cpsaicontact@gmail.com>
2024-01-27 17:44:43 -0500

    wip

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,6 @@ FROM apache/airflow:2.8.1
 
 # using git sync
 # COPY --chown=airflow:root ./dags/ ${AIRFLOW_HOME}/dags/
+
+# should probably pin a python-specific version of airflow
+RUN pip install -e .
--- dags/it_be_me_dag.py
+++ dags/it_be_me_dag.py
@@ -1,3 +1,4 @@
+import time
 from datetime import datetime
 
 from airflow import DAG
@@ -15,5 +16,67 @@ with DAG(dag_id='yar_be_the_dag', start_date=datetime(2024, 1, 27), schedule="* 
     def sample_python_task_2():
         print('this was printed from a python task that ran after the test_operator')
 
+    @task()
+    def sample_python_task_3():
+        print('this was printed from a python task that ran after the test_operator')
 
-    sample_python_task() >> sample_python_task_2()
+    @task()
+    def sample_python_task_4():
+        print('this was printed from a python task that ran after the test_operator')
+
+    @task()
+    def sample_python_task_5():
+        print('this was printed from a python task that ran after the test_operator')
+        time.sleep(15)
+        print('haha, waited')
+
+    @task()
+    def sample_python_task_6():
+        print('this was printed from a python task that ran after the test_operator')
+
+    @task()
+    def sample_python_task_7():
+        print('this was printed from a python task that ran after the test_operator')
+
+    @task()
+    def sample_python_task_8():
+        print('this was printed from a python task that ran after the test_operator')
+
+    @task()
+    def sample_python_task_9():
+        print('this was printed from a python task that ran after the test_operator')
+
+    @task()
+    def sample_python_task_10():
+        print('this was printed from a python task that ran after the test_operator')
+
+    @task()
+    def sample_python_task_11():
+        print('this was printed from a python task that ran after the test_operator')
+
+    @task()
+    def sample_python_task_12():
+        print('this was printed from a python task that ran after the test_operator')
+
+    @task()
+    def sample_python_task_13():
+        print('this was printed from a python task that ran after the test_operator')
+
+    @task()
+    def sample_python_task_14():
+        print('this acts as a collector task of sorts')
+
+    @task()
+    def sample_python_task_15():
+        print('this was printed from a python task that ran after the test_operator and is the last task')
+
+
+    intro_chain = sample_python_task() >> sample_python_task_2() >> sample_python_task_9()
+
+    divergent_1 = sample_python_task_2() >> [sample_python_task_3(), sample_python_task_4(), sample_python_task_5()] >> sample_python_task_6() >> sample_python_task_7()
+
+    divergent_2 = [sample_python_task_3(), sample_python_task_4(), intro_chain] >> sample_python_task_8()
+
+    sub_divergent = divergent_2 >> [sample_python_task_10(), sample_python_task_11(), sample_python_task_12(), sample_python_task_13()] >> sample_python_task_14()
+
+    [divergent_1, sub_divergent] >> sample_python_task_15()
