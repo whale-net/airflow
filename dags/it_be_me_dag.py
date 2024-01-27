7fec739bc68b898ce66d2f7fa539081d9605dd0c
qmastermoo <cpsaicontact@gmail.com>
2024-01-27 18:21:45 -0500

    fix

--- dags/it_be_me_dag.py
+++ dags/it_be_me_dag.py
@@ -12,15 +12,15 @@ with DAG(dag_id='yar_be_the_dag', start_date=datetime(2024, 1, 27), schedule="* 
     def sample_python_task():
         print('this was printed from a python task')
 
-    @task(multiple_outputs=True)
+    @task()
     def sample_python_task_2():
         print('this was printed from a python task that ran after the test_operator')
 
-    @task(multiple_outputs=True)
+    @task()
     def sample_python_task_3():
         print('this was printed from a python task that ran after the test_operator')
 
-    @task(multiple_outputs=True)
+    @task()
     def sample_python_task_4():
         print('this was printed from a python task that ran after the test_operator')
 
@@ -73,11 +73,15 @@ with DAG(dag_id='yar_be_the_dag', start_date=datetime(2024, 1, 27), schedule="* 
 
     #sample_python_task() >> sample_python_task_2() >> sample_python_task_9() >> [sample_python_task_3(), sample_python_task_4()] >> sample_python_task_8()
 
-    intro_chain = sample_python_task() >> sample_python_task_2() >> sample_python_task_9()
 
-    divergent_1 = sample_python_task_2() >> [sample_python_task_3(), sample_python_task_4(), sample_python_task_5()] >> sample_python_task_6() >> sample_python_task_7()
+    t2 = sample_python_task_2()
+    t3 = sample_python_task_3()
+    t4 = sample_python_task_4()
 
-    divergent_2 = [sample_python_task_3(), sample_python_task_4(), intro_chain] >> sample_python_task_8()
+    intro_chain = sample_python_task() >> t2 >> sample_python_task_9()
+    divergent_1 = t2 >> [t3, t4, sample_python_task_5()] >> sample_python_task_6() >> sample_python_task_7()
+
+    divergent_2 = [t3, t4, intro_chain] >> sample_python_task_8()
 
     sub_divergent = divergent_2 >> [sample_python_task_10(), sample_python_task_11(), sample_python_task_12(), sample_python_task_13()] >> sample_python_task_14()
 
