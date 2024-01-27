c66f7444c4b64ff9c55f83daa72e59c904c4a6bd
qmastermoo <cpsaicontact@gmail.com>
2024-01-27 00:18:50 -0500

    add test file

---
+++ dags/test.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.bash import BashOperator
+
+with DAG(dag_ig='test_dag', start_date=datetime(2024, 1, 27), schedule="* * * * *"):
+
+
+    test_operator = BashOperator(task_id="sample_bash_operator", bash_command="echo this is a bash operator")
+
+    @task()
+    def sample_python_task():
+        print('this was printed from a python task')
+
+    @task()
+    def sample_python_task_2():
+        print('this was printed from a python task that ran after the test_operator')
+
+
+    sample_python_task() >> test_operator >> sample_python_task_2()
