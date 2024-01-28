38ddd487aebe020d9b89df66fdb06336bcafa01d
qmastermoo <cpsaicontact@gmail.com>
2024-01-27 19:38:58 -0500

    disable catchup

--- dags/it_be_me_dag.py
+++ dags/it_be_me_dag.py
@@ -6,7 +6,7 @@ from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 
 #with DAG(dag_id='test_dag', start_date=datetime(2024, 1, 27), schedule="* * * * *"):
-with DAG(dag_id='yar_be_the_dag', start_date=datetime(2024, 1, 27), schedule="* * * * *") as dag:
+with DAG(dag_id='yar_be_the_dag', start_date=datetime(2024, 1, 27), schedule="* * * * *", catchup=False) as dag:
 
     @task()
     def sample_python_task():
